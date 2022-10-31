700de0348d04c2d490c9075b9745a49f854e3cad
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-11-01 01:55:23 +0500

    5-misol

--- bin/string_indexing05.dart
+++ bin/string_indexing05.dart
@@ -1,4 +1,14 @@
 String func(String s) {
+  int b = s.length;
+  int c = b % 2;
+  if (c == 1) {
+    return s[b ~/ 2];
+  } else {
+    int d = b ~/ 2;
+    int r = (b ~/ 2) - 1;
+    String e = s[r] + s[d];
+    return e;
+  }
   """
     A string is given. Find character in the middle.
     if string length is even return two character in the middle
@@ -8,5 +18,9 @@ String func(String s) {
         str: answer
     """;
   //your code here
-  return '';
+
+}
+
+void main() {
+  print(func("0-fud80uo239u8fiop"));
 }
