492a29b0c4a24cfcf7f8bdc7ca09f3d6ecef2ca3
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-11-01 02:09:27 +0500

    4-misol

--- bin/string_indexing04.dart
+++ bin/string_indexing04.dart
@@ -1,4 +1,8 @@
 String func(String s) {
+  String a = s[0];
+  String b = s[1];
+  String c = s[2];
+  return a+b+c;
   """
     The s string variable is given. Return three characters from the beginning.
     Args:
@@ -8,4 +12,8 @@ String func(String s) {
     """;
   //your code here
   return '';
+}
+
+void main() {
+  print(func("oktam"));
 }
