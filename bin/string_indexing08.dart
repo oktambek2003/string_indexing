9bba5c869408e1a6f16e5dd9b240e3cc5de938f1
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-11-01 14:28:48 +0500

    8-misol

--- bin/string_indexing08.dart
+++ bin/string_indexing08.dart
@@ -1,4 +1,27 @@
 int func(String s) {
+  String a = s[0];
+  String b = s[1];
+  String c = s[2];
+  String d = s[3];
+  String e = s[4];
+  String h = "*";
+  if (a == h) {
+    return 0;
+  }
+  if (b == h) {
+    return 1;
+  }
+  if (c == h) {
+    return 2;
+  }
+  if (d == h) {
+    return 3;
+  }
+  if (e == h) {
+    return 4;
+  } else {
+    return 0;
+  }
   """
     A string of length five is given. Return the index of the "*" character, 
     return 0 if not present.
@@ -9,4 +32,8 @@ int func(String s) {
     """;
   //your code here
   return -1;
+}
+
+void main() {
+  print(func("okta*"));
 }
